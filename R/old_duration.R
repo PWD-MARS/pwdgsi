f5f524fdd85c1046dc9af7073746302df64b5622
Jon Bryant <jon@jonbryant.co>
2025-05-19 09:46:28 -0400

    Update file names/locations

--- R/old_duration.R
+++ R/old_duration.R
@@ -21,7 +21,6 @@ old_duration <- function(dtime_est) {
   event_start <- dtime_est[1]
   #Notes: Assumes 15-minute time increments. As written, code should 
   #only be applied to ONE EVENT at a time
-  
   duration_calc <- difftime(dtime_est[length(dtime_est)], event_start, units = "hours")
   duration <- as.double(duration_calc) + 0.25 
   #15-minutes added to account for time subtraction
