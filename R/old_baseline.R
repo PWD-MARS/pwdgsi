465542107ffff69141bda3ecf239ea8e7c78f21a
Jon Bryant <jon@jonbryant.co>
2025-05-21 15:42:05 -0400

    Updated and validated marsWaterLevelBaseline
    Opened additional ticket for potential issues with the function in #33

--- R/old_baseline.R
+++ R/old_baseline.R
@@ -10,26 +10,25 @@ old_baseline <- function(dtime_est, level_ft, max_infil_rate_inhr = 1){
   }else{ #15 minute interval
     steps <- 1
   }
-  level_ft
-  # 
-  # #create dataframe of dtime_est and level, and discard NAs formed at the edges during the moving average
-  # df <- data.frame(dtime_est, level_ft) %>% dplyr::filter(!is.na(level_ft))
-  # 
-  # last_point <- round(df$level_ft[length(df$level_ft)], 4)
-  # 
-  # test <- df %>% #Check if difference between level over x timesteps is less than 0.01ft
-  #   dplyr::mutate(lag_1 = dplyr::lag(level_ft, steps), 
-  #                 diffr = abs(level_ft - lag_1)) %>% 
-  #   dplyr::arrange(desc(dtime_est)) %>% 
-  #   head(5)
-  # 
-  # #0.25 in/hr or lower is considered "not infiltrating". This is converted to ft/(15 minutes)
-  # depth_change <- max_infil_rate_inhr*1/12*15/60
-  # 
-  # if(min(test$diffr, na.rm = TRUE) > depth_change){
-  #   return(NA)
-  # }else{
-  #   return(round(last_point, 4))
-  # }
-  
+
+  #create dataframe of dtime_est and level, and discard NAs formed at the edges during the moving average
+  df <- data.frame(dtime_est, level_ft) %>% dplyr::filter(!is.na(level_ft))
+
+  last_point <- round(df$level_ft[length(df$level_ft)], 4)
+
+  test <- df %>% #Check if difference between level over x timesteps is less than 0.01ft
+    dplyr::mutate(lag_1 = dplyr::lag(level_ft, steps),
+                  diffr = abs(level_ft - lag_1)) %>%
+    dplyr::arrange(desc(dtime_est)) #%>%
+  head(5)
+
+  #0.25 in/hr or lower is considered "not infiltrating". This is converted to ft/(15 minutes)
+  depth_change <- max_infil_rate_inhr*1/12*15/60
+
+  if(min(test$diffr, na.rm = TRUE) > depth_change){
+    return(NA)
+  }else{
+    return(round(last_point, 4))
+  }
+
 }
