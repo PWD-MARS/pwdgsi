99c06dfe7afd13e99e8eff9781e28b53afde44ef
Ryan Ebrahimi <Ryan.Ebrahimi@phila.gov>
2025-11-14 11:19:54 -0500

    wrap

--- theory_files/test_discharge.R
+++ theory_files/test_discharge.R
@@ -11,6 +11,13 @@ library(tidyverse)
 library(lubridate)
 # smoothing library
 library(forecast)
+# plot
+library(ggplot2)
+library(splines)
+library(patchwork)
+
+
+
 
 # DB connections & functions
 poolConn <- dbPool(RPostgres::Postgres(),
@@ -18,7 +25,8 @@ poolConn <- dbPool(RPostgres::Postgres(),
                    port = 5434,
                    dbname = "mars_prod",
                    user = Sys.getenv("admin_uid"),
-                   password = Sys.getenv("admin_pwd")
+                   password = Sys.getenv("admin_pwd"),
+                   timezone = NULL
 )
 
 # smp-id 1359-8-2 is a trench with prior deployments. The orifice sits at 12 inches above the stone with 0.5 inch diameter and storage volume: 833 ft3
@@ -75,14 +83,16 @@ marsUnderdrainOutflow_cf <- function(dtime_est,
 }
 
 # pull water leevl data
-level_data <- dbGetQuery(poolConn, "SELECT * from data.viw_ow_leveldata_sumpcorrected 
-                         where smp_id = '1359-8-2'")
+level_data <- dbGetQuery(poolConn, "SELECT * from data.tbl_ow_leveldata_raw 
+                         where ow_uid = 1738")
 
 # single out storm on Aug 9 2024
 level_df <- level_data %>% 
-  dplyr::filter(dtime > as.Date("2024/08/08") & dtime < as.Date("2024/08/19"))
+  dplyr::filter(dtime > as.Date("2024/08/08") & dtime < as.Date("2024/08/19")) %>%
+  dplyr::distinct()
 level_df_storm <- level_data %>% 
-  dplyr::filter(dtime > as.Date("2024/08/08") & dtime < as.Date("2024/08/10"))
+  dplyr::filter(dtime > as.Date("2024/08/08") & dtime < as.Date("2024/08/10")) %>%
+  dplyr::distinct()
 
 
 plot(level_df$dtime, level_df$level_ft)
@@ -127,12 +137,60 @@ fit_loess <- loess(level_df_clean$level_ft ~ level_df_clean$dtime_num)
 smoothed_loess <- predict(fit_loess)
 
 
+# using spline method
+
+# Convert time to numeric (if it's POSIXct)
+level_df_storm$time_num <- as.numeric(level_df_storm$dtime)
+
+# Fit a natural spline model
+spline_fit <- lm(level_ft ~ ns(time_num, df = 50), data = level_df_storm)
+
+# Predict smoothed values
+level_df_storm$level_spline <- predict(spline_fit)
+
+# alternative method
+# Convert time to numeric
+time_num <- as.numeric(level_df_storm$dtime)
+
+# Fit smoothing spline
+spline_model <- smooth.spline(x = time_num, y = level_df_storm$level_ft, spar = 0.3)
+
+# can also use cross-validation for picking spar
+#spline_model <- smooth.spline(x = time_num, y = level_df_storm$level_ft, cv = TRUE)
+
+# Predict smoothed values- spline
+level_df_storm$level_spline <- predict(spline_model, x = time_num)$y
+
+# Predict smoothed values- moving ave
+level_df_storm$moving_a_five_level <- rollmean(level_df_storm$level_ft, k = 5, fill = NA)
+
+# Calculate a 3-period center-aligned rolling median
+# k specifies the window width (must be an odd integer for center alignment)
+level_df_storm$moving_median_three_level <- rollmedian(level_df_storm$level_ft, k = 5, fill = NA)
 
 
+spline <- ggplot(level_df_storm, aes(x = dtime)) +
+            geom_point(aes(y = level_ft), color = "black") +
+            geom_line(aes(y = level_spline), color = "blue") +
+            #geom_line(aes(y = moving_a_five_level), color = "red") +
+            #geom_line(aes(y = moving_median_three_level), color = "purple") +
+            labs(title = "Spline Fit for Water Level", y = "Level (ft)", x = "Time")
+          
+ma5 <- ggplot(level_df_storm, aes(x = dtime)) +
+            geom_point(aes(y = level_ft), color = "black") +
+            #geom_line(aes(y = level_spline), color = "blue") +
+            geom_line(aes(y = moving_a_five_level), color = "red") +
+            #geom_line(aes(y = moving_median_three_level), color = "purple") +
+            labs(title = "Moving Ave of 5 Fit for Water Level", y = "Level (ft)", x = "Time")
+          
+
+mm3 <- ggplot(level_df_storm, aes(x = dtime)) +
+            geom_point(aes(y = level_ft), color = "black") +
+            #geom_line(aes(y = level_spline), color = "blue") +
+            #geom_line(aes(y = moving_a_five_level), color = "red") +
+            geom_line(aes(y = moving_median_three_level), color = "purple") +
+            labs(title = "Moving Median of 3 Fit for Water Level", y = "Level (ft)", x = "Time")
 
 
-
-
-
-
+combined_smooth <- spline/ma5/mm3
 
